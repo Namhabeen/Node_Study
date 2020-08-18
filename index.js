94d2b5c72213a4e86460fd1a58179af87a123326
South_under_been <s2018s27@e-mirim.hs.kr>
2020-08-18 22:04:10 +0900

    add body-parser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express=require('express')
 const mongoose=require('mongoose')
+const bodyParser=require('body-parser');
 const app=express()
 const port=3000
+const {User}=require("./models/user");
+
+//body-parser application 분석 (9-11)
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://happybeen:abcd1234@jsstudy.smv9w.mongodb.net/<dbname>?retryWrites=true&w=majority',{
     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true, useFindAndModify:false
@@ -9,5 +16,16 @@ mongoose.connect('mongodb+srv://happybeen:abcd1234@jsstudy.smv9w.mongodb.net/<db
   .catch(err=>console.log(err))
 
 app.get('/',(req,res)=>res.send('Hello World!'))
+
+app.post('/register',(req,res)=>{
+    //회원가입시 필요한 정보들을 client로 부터 가져와 DB에 넣어줌
+    const user=new User(req.body)
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(port,()=>console.log('Connected'))
