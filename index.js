b23b21f476984be78ec22b35028503ce4129ed20
South_under_been <s2018s27@e-mirim.hs.kr>
2020-08-19 11:46:09 +0900

    add npm_bcrypt&token

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
 
+app.get('/',(req,res)=>res.send('Hello World!'))
+
+app.listen(port,()=>console.log('Connected'))
+
 mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true, useFindAndModify:false
 }).then(()=>console.log('Mongo DB Connected...'))
   .catch(err=>console.log(err))
-
-app.get('/',(req,res)=>res.send('Hello World! node mon test'))
-
-app.listen(port,()=>console.log('Connected'))
 
 app.post('/register',(req,res)=>{
     //회원가입시 필요한 정보들을 client로 부터 가져와 DB에 넣어줌
@@ -34,7 +34,7 @@ app.post('/register',(req,res)=>{
 
 app.post('/login',(req,res)=>{
     //가입 된 이메일인지 확인
-    User.findOne({email: req.body.email},(err,user)=>{
+    user.findOne({email: req.body.email},(err,user)=>{
         if(!userInfo){
             return req.json({
                 loginSuccess:false,
